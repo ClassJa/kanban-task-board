3c3017dbaaa79fee31dd0bd7816695d9b85918f8
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-01 19:09:21 -0500

    working through creating a modal styled task pop-up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const addTaskBtn = document.querySelector('.addTaskBtn')
 
 projectTask = {
     // id: Int32Array,
@@ -9,6 +10,31 @@ projectTask = {
     // dueDate: $(''),
     // taskStatus: $('')
 }
+
+
+// taskCard = {
+//     id: $(''),
+//     title: $(''),
+//     desc: $(''),
+//     dueDate: $(''),
+//     taskStatus: $('')
+// }
+
+
+// TODO: Create a new card element and add the classes `card`, `project-card`, `draggable`, and `my-3`. Also add a `data-project-id` attribute and set it to the project id.
+  // TODO: Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
+  // TODO: Create a new card body element and add the class `card-body`.
+  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
+  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
+  // TODO: Create a new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
+
+addTaskBtn.addEventListener('click', function(){
+    // open up the modal and allow for user to input content in the box's text field
+    console.log("click")
+})
+
+
+
 
 
 // Todo: create a function to generate a unique task id
@@ -23,19 +49,21 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    document.createElement('card')
+    const taskCard = $('<div>')
+    const cardHeader = $('<div>')
+    cardHeader.addClass('card-header', 'draggable')
+    // cardHeader.createElement('class', '.card-header')
+    cardHeader.textContent = "This is the header"
+    taskCard.append(cardHeader)
+    // document.createElement('card')
+    console.log(taskCard)
 
-    taskCard = {
-        id: $(''),
-        title: $(''),
-        desc: $(''),
-        dueDate: $(''),
-        taskStatus: $('')
-    }
+    // how to incorporate the task parameter? 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+
 
 }
 
