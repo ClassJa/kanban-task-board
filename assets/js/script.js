295cf20e3e7a5f1893e900372a9ef23d19732d89
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-03 23:04:51 -0500

    Refactoring code to be in Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+// make this an array of objects
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+// save the value of tasks as an object
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// create an object for 6-8
 let taskTitle = JSON.parse(localStorage.getItem("taskTitle"));
 let taskDueDate = JSON.parse(localStorage.getItem("taskDueDate"));
 let taskDescription = JSON.parse(localStorage.getItem("taskDescription"));
 // let = JSON.parse(localStorage.getItem(""));
 // let = JSON.parse(localStorage.getItem(""));
 let toDoColumn = document.getElementById('to-do-column');
+console.log(toDoColumn)
+console.log($('#to-do-column'))
 let inProgressColumn = document.getElementById('in-progress-column');
 let doneColumn = document.getElementById('done-column')
 
@@ -207,60 +213,101 @@ function generateTaskId() {
 function createTaskCard(task) {
     // task is the object with many properties
     console.log(task)
+
+    // const t = $('div')
     // const taskCard = $('<div>')
-    const taskCard = document.createElement('div')
+    const taskCard = $('div')
+    taskCard.addClass('draggable card-header')
     console.log(taskCard)
     // const cardHeader = $('<h2>')
-    const cardHeader = document.createElement('h2')
+    const cardHeader = $('h2')
     console.log(cardHeader)
     // const cardBody = $('<div>')
-    const cardBody = document.createElement('div')
+    const cardBody = $('div')
     console.log(cardBody)
     // const cardButton = $('<button>')
-    const cardButton = document.createElement('button')
+    const cardButton = $('button')
     console.log(cardButton)
     // taskCard.addClass('draggable')
     taskCard.setAttribute('class', 'draggable card-header')
     // cardHeader.addClass('card-header', 'draggable')
     // cardHeader.createElement('class', '.card-header')
    
-    cardHeader.textContent = task.title.value
-    cardBody.textContent = task.desc.value
+    cardHeader.text = task.title
+    console.log(cardHeader.textContent)
+    cardBody.textContent = task.desc
+    console.log(cardBody.textContent)
 
-    taskCard.appendChild(cardHeader)
-    taskCard.appendChild(cardBody)
-    taskCard.appendChild(cardButton)
+    taskCard.append(cardHeader)
+    taskCard.append(cardBody)
+    taskCard.append(cardButton)
 
     // task.appendChild(taskCard)
-    console.log(task)
 
+    // t.appendChild(taskCard)
+    console.log('This is the t', t)
+    console.log('This is the task', task)
+    
     
     // taskCard.append(cardHeader, cardBody, cardButton)
     // document.createElement('card')
     // console.log(taskCard)
 
-    return task
+    return taskCard
 }
+// call this in renderTL function 
 createTaskCard(task)
+console.log(task)
 
 
 // Todo: create a function to render the task list and make cards draggable
 
-taskList = []
+
 function renderTaskList() {
-    if (taskList > 0) {
-        for (let i = 0; i <taskList.length; i ++){
-                if (taskList[i].taskStatus === toDoColumn) {
-                    toDoColumn.appendChild(taskList[i])
-                } else if (taskList[i].taskStatus === inProgressColumn) {
-                    inProgressColumn.appendChild(taskList[i])
-                } else {
-                    doneColumn.appendChild(taskList[i])
-                    // taskList[i] === doneColumn
-                }
-            }
+    // empty the taskCard by targeting each column 
+    for (let task of taskList){
+            if (task.taskStatus === 'to-do') {
+                toDoColumn.append(createTaskCard(task))
+            } else if (task.taskStatus === 'in-progress') {
+                inProgressColumn.append(task)
+                // figure out how to append in JQUERY
+            } else {
+                doneColumn.append(task)
+                // taskList[i] === doneColumn
+
         }
+    }
+
+
+    // for (let project of projects) {
+    //     if (project.status === 'to-do') {
+    //       todoList.append(createProjectCard(project));
+    //     } else if (project.status === 'in-progress') {
+    //       inProgressList.append(createProjectCard(project));
+    //     } else if (project.status === 'done') {
+    //       doneList.append(createProjectCard(project));
+    //     }
+    //   }
+
+    $('.draggable').draggable({
+        opacity: 0.7,
+        zIndex: 100,
+        // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+        helper: function (e) {
+          // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+          const original = $(e.target).hasClass('draggable')
+            ? $(e.target)
+            : $(e.target).closest('.draggable');
+          // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+          return original.clone().css({
+            width: original.outerWidth(),
+          });
+        },
+      });
+    // add a draggable class when the card is created 
         
+
+        // when dropped in one of the swim-lanes change the taskStatus accordingly 
 
        // place ids on the 3 columns to check its status
         // taskList = []
@@ -295,7 +342,7 @@ function renderTaskList() {
 
 
 }
-renderTaskList()
+// renderTaskList()
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
@@ -308,16 +355,27 @@ function handleAddTask(event){
     title: $('#taskTitle').val(),
     desc: $('#taskDescription').val(),
     dueDate: $('#taskDueDate').val(),
-    taskStatus: $('').val()
+    taskStatus: 'to-do'
     // figure out taskStatus id 
     }
     taskList.push(task)
 
-for (let i = 0; i < taskList.length; i++){
-    console.log("Items in the task list", taskList[i])
-    localStorage.setItem('tasks', JSON.stringify(task))
+
+    // stringify allows for saving to local storage
+    // json.parse allows the revert of the string to its original state 
+    localStorage.setItem('tasks', JSON.stringify(taskList))
+    $('#taskTitle').val(""),
+    $('#taskDescription').val(""),
+    $('#taskDueDate').val(""),
+    console.log(JSON.stringify(taskList))
+
     renderTaskList()
-}
+
+    // may need to use this for loop later
+// for (let i = 0; i < taskList.length; i++){
+//     console.log("Items in the task list", taskList[i])
+//     renderTaskList()
+// }
 
 // uncomment soon 
 // console.log("Items in the task list", taskList)
@@ -356,7 +414,9 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+ // when dropped in one of the swim-lanes change the taskStatus accordingly 
+//  event.target.id (todo, inprogress, done) -> (gets the location) use this to set the task.taskStatus = 
+// target the event that its dropped on and get it's id set the status of the task to the column its dropped on 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
